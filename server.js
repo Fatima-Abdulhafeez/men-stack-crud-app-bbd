db9b2a6159729f474c1afbe029d380718ef1c6cc
fatimahz-ga <fatimaabdullhafeez@gmail.com>
2025-07-13 16:02:46 +0300

    added ejs,css

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const app = express();
+const path = require("path");
+
+//midlleware
+app.use(express.static(path.join(__dirname, "public")));
 
 //Get / Routs
 app.get('/',(req,res)=>{
-   res.send('Hello Friend!');
+   res.render('index.ejs')
 });
 
 app.listen(3000,()=>{
