0de64f51ee996ca9376f4f3e53a4fdc83b7d744d
fatimahz-ga <fatimaabdullhafeez@gmail.com>
2025-07-14 09:25:58 +0300

    added database connection

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const dotenv = require("dotenv"); 
+dotenv.config();
 const express = require('express');
-
+const { connect } = require("http2");
+const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
+
+//database connection
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});// log connection status to terminal on start
 
 //midlleware
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,6 +21,3 @@ app.get('/',(req,res)=>{
    res.render('index.ejs')
 });
 
-app.listen(3000,()=>{
-    console.log('Listening on port 3000');
-});
